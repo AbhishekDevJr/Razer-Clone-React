20dd6f306d11c9603db7459e6ffc31824af5baa3
Abhishek Choudhari <abhishek007coc@gmail.com>
2023-01-18 22:56:01 +0530

    Feat:- UI-Footer development start

---
+++ razer-clone-react/src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Component} from 'react';
+
+class Footer extends Component {
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return(
+            <footer className = "foot">
+                This will be footer
+            </footer>
+        );
+    }
+}
